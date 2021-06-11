24a58b8fca89a44c385966e497d4523146e54772
Matthew Miller <matthew@millerti.me>
2021-06-11 08:14:33 -0700

    Add TODO about deprecated `createCipher()`

--- app/lib/encryption.js
+++ app/lib/encryption.js
@@ -3,6 +3,17 @@ const crypto = require('crypto')
 const algorithm = 'aes-256-ctr'
 
 module.exports = {
+    /**
+     * TODO: `crypto.createCipher()` is stupid deprecated, but I don't use any of this so I'm
+     * not spending any time fixing it. It'll eventually need to be replaced with
+     * `crype.createCipheriv()`:
+     *
+     * https://nodejs.org/api/crypto.html#crypto_crypto_createcipheriv_algorithm_key_iv_options
+     *
+     * I tried to quickly swap it out but stopped when I couldn't figure out of the `iv` argument
+     * needs to be persisted for something encrypted with it to be decrypted (I suspect it does,
+     * and if that's the case I don't want to engineer a solution for that right now)
+     */
     encrypt(readableStream, password) {
         const cipher = crypto.createCipher(algorithm, password)
         return readableStream.pipe(cipher)
